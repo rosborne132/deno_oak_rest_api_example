80574d7c1ad9c740917f52b7d6963886e5b827d2
rosborne132 <ozborne132@gmail.com>
2020-05-18 01:13:41 -0700

    Add addTodo post route

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
-import { getFolders, getTodos } from "./src/controllers/index.ts";
+import { getFolders, addTodo, getTodos } from "./src/controllers/index.ts";
 
 const router = new Router();
+const port = 3000;
 
 router
   .get("/", (context) => {
     context.response.body = "Hello";
   })
   .get("/todos", getTodos)
+  .post("/todos", addTodo)
   .get("/folders", getFolders);
 //     .get('/book/:id', context => {
 //         if (
@@ -24,4 +26,14 @@ const app = new Application();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
-await app.listen({ port: 8000 });
+
+// Logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+console.log(`Listening on port ${port}...`);
+
+await app.listen({ port });
--- src/controllers/todos/todos.ts
+++ src/controllers/todos/todos.ts
@@ -1,10 +1,33 @@
-// import { v4 } from 'https://deno.land/std/uuid/mod.ts'
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
 import { Todos } from "../../model/index.ts";
+import { Todo } from "../../types/index.ts";
 
 export const getTodos = ({ response }: { response: any }) => {
-  // Generate a v4 uuid
-  //const myUUID = v4.generate()
-
+  response.status = 200;
   response.body = Todos;
+  return;
+};
+
+export const addTodo = async ({
+  request,
+  response,
+}: {
+  request: any;
+  response: any;
+}) => {
+  const body = await request.body();
+  const { name, folderId } = await JSON.parse(body.value);
+
+  const todo: Todo = {
+    id: v4.generate(),
+    name,
+    isComplete: false,
+    folderId,
+  };
+
+  Todos.push(todo);
+
+  response.body = { msg: "Todo added!" };
+  response.status = 200;
 };
